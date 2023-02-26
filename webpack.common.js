2896069a97a874e38bb9004fc6a599b29609ab9d
FlyingThaCat <2021071031@student.pppkpetra.sch.id>
2023-02-26 23:46:03 +0700

    fix pwa

--- webpack.common.js
+++ webpack.common.js
@@ -20,27 +20,6 @@ module.exports = {
     minimizer: [
       new CssMinimizerPlugin(),
     ],
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 70000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      automaticNameDelimiter: '~',
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
   },
   module: {
     rules: [
